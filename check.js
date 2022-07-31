0b8701cbe2636114ad5a0e9e0f269f5029b8a3fd
your-sdnss <maxim32rp@gmail.com>
2022-07-31 17:08:15 +0300

    d

--- check.js
+++ check.js
@@ -4,10 +4,9 @@ const token = process.env.API_TOKEN;
 const fs = require("fs");
 const cronJob = require('cron').CronJob;
 
-const job = new cronJob('0 */10 * * * *', getData);
+// const job = new cronJob('0 0 */1 * * *', getData);
 
-job.start();
-
+// job.start();
 
 const instance = axios.create({
     baseURL: 'https://api.bigbuy.eu/rest',
@@ -93,30 +92,19 @@ const stockData = instance.get('/catalog/productsstock');
 
 const variableData = instance.get('/catalog/productsvariationsstock');
 
+getData();
+
 function getData() {
     axios.all([stockData, variableData]).then(axios.spread((...responses) => {
 
         let dateRaw = new Date();
         let dateNow = dateRaw.toLocaleString('en-GB', {timeZone: 'Europe/Kiev'});
-        let yearNow = dateNow.split("/")[2].split(",")[0];
-        let monthNow = dateNow.split("/")[1];
-        let dayNow = dateNow.split("/")[0];
-        let hourNow = dateNow.split("/")[2].split(",")[1].replace(/\s/gm, "").split(":")[0];
+        let hour = dateNow.split("/")[2].split(",")[1].replace(/\s/gm, "").split(":")[0];
 
-        let checkingArr = [];
-
-        let firstNum = hourNow.split("")[0];
-        let secondNum = hourNow.split("")[1];
+        let firstNum = hour.split("")[0];
+        let secondNum = hour.split("")[1];
 
         let hourMin;
-
-        function getIntersection(array1, array2) {
-            return array1.filter(object1 => {
-                return array2.some(object2 => {
-                    return object1.sku === object2.sku;
-                });
-            });
-        }
 
         if (firstNum === "0") {
             secondNum -= 1;
@@ -124,154 +112,20 @@ function getData() {
         } else if (firstNum === "1" && secondNum === "0") {
             hourMin = "09";
         } else {
-            hourMin = hourNow - 1;
+            hourMin = hour - 1;
         }
 	    console.log(hourMin);
 
         const stockResponse = responses[0].data;
         const variableResponse = responses[1].data;
 
-        for (let id1 of stockResponse) {
-            let obj = {
-                sku: "",
-                quantity: 0
-            }
-            obj.sku = id1.sku;
-            obj.quantity = id1.stocks[0].quantity;
+        let base = checkBase(stockResponse, dateNow, hourMin);
+        let variables = checkVariables(variableResponse, dateNow, hourMin);
+        const all = [...base, ...variables];
 
-            checkingArr.push(obj);
-        }
+        all.sort((a,b) => a.diff - b.diff);
 
-        fs.writeFileSync(`./${yearNow}-${monthNow}-${dayNow}T${hourNow}.json`, JSON.stringify(checkingArr));
-
-        let rawBase;
-
-        const getYesterday = (dateOnly = false) => {
-            let d = new Date();
-            d.setDate(d.getDate() - 1);
-              d = d.toLocaleString('en-GB', {timeZone: 'Europe/Kiev'});
-            return dateOnly ? new Date(d.toDateString()) : d;
-        };
-
-        let dayY = getYesterday().split("/")[0];
-
-        if (hourNow.toString() === "00") {
-            rawBase = fs.readFileSync(`${yearNow}-${monthNow}-${dayY}T23.json`);
-        } else if (hourNow.toString() === "01") {
-            rawBase = fs.readFileSync(`${yearNow}-${monthNow}-${dayNow}T00.json`);
-        } else {
-            console.log(hourNow);
-            rawBase = fs.readFileSync(`${yearNow}-${monthNow}-${dayNow}T${hourMin}.json`);
-        }
-
-        let baseArr = JSON.parse(rawBase);
-
-        var biggestKey;
-
-        if (baseArr.length > checkingArr.length) {
-            biggestKey = baseArr.length;
-        } else {
-            biggestKey = checkingArr.length;
-        }
-
-        console.log("Difference");
-
-        let baseArrNew = getIntersection(baseArr, checkingArr)
-        let checkingArrNew = getIntersection(checkingArr, baseArr)
-
-        baseArrNew.sort((a, b) => (a.sku > b.sku) ? 1 : ((b.sku > a.sku) ? -1 : 0));
-        checkingArrNew.sort((a, b) => (a.sku > b.sku) ? 1 : ((b.sku > a.sku) ? -1 : 0));
-
-
-        let reqArray = []
-
-        for (let j = 0; j < biggestKey; j++) {
-            if (baseArrNew[j] !== undefined && checkingArrNew[j] !== undefined) {
-                if (baseArrNew[j].quantity > checkingArrNew[j].quantity) {
-                    let reqObject = {
-                        sku: "",
-                        diff: 0,
-                        quantity: 0
-                    }
-                    reqObject.sku = checkingArrNew[j].sku;
-                    reqObject.quantity = checkingArrNew[j].quantity;
-                    reqObject.diff = baseArrNew[j].quantity - checkingArrNew[j].quantity;
-                    reqArray.push(reqObject);
-                    console.log(reqObject);
-                }
-            }
-        }
-
-        console.log(reqArray);
-
-
-        //variables start
-
-
-        console.log("variables");
-
-        let varCheckingArr = [];
-
-        for (let id2 of variableResponse) {
-            let obj = {
-                sku: "",
-                quantity: 0
-            }
-            obj.sku = id2.sku;
-            obj.quantity = id2.stocks[0].quantity;
-
-            varCheckingArr.push(obj);
-        }
-
-        fs.writeFileSync(`./${yearNow}-${monthNow}-${dayNow}T${hourNow}-variables.json`, JSON.stringify(varCheckingArr));
-
-        console.log("variables-ready")
-
-        let rawCheckBase;
-
-        if (hourNow.toString() === "00") {
-            rawCheckBase = fs.readFileSync(`${yearNow}-${monthNow}-${dayY}T23-variables.json`)
-        } else if (hourNow.toString() === "01") {
-            rawCheckBase = fs.readFileSync(`${yearNow}-${monthNow}-${dayNow}T00-variables.json`);
-        } else {
-            rawCheckBase = fs.readFileSync(`${yearNow}-${monthNow}-${dayNow}T${hourMin}-variables.json`);
-        }
-        let checkBaseArr = JSON.parse(rawCheckBase);
-
-        console.log("difference")
-
-        let varBaseInt = getIntersection(checkBaseArr, varCheckingArr)
-        let varCheckInt = getIntersection(varCheckingArr, checkBaseArr)
-
-        varBaseInt.sort((a, b) => (a.sku > b.sku) ? 1 : ((b.sku > a.sku) ? -1 : 0));
-        varCheckInt.sort((a, b) => (a.sku > b.sku) ? 1 : ((b.sku > a.sku) ? -1 : 0));
-
-        let varBiggestKey;
-
-        if (varBaseInt.length > varCheckInt.length) {
-            varBiggestKey = varBaseInt.length;
-        } else {
-            varBiggestKey = varCheckInt.length;
-        }
-
-        for (let j = 0; j < varBiggestKey; j++) {
-            if (varBaseInt[j] !== undefined && varCheckInt[j] !== undefined) {
-                if (varBaseInt[j].quantity > varCheckInt[j].quantity) {
-                    let varReqObject = {
-                        sku: "",
-                        diff: 0,
-                        quantity: 0
-                    }
-                    varReqObject.sku = varCheckInt[j].sku;
-                    varReqObject.quantity = varCheckInt[j].quantity;
-                    varReqObject.diff = varBaseInt[j].quantity - varCheckInt[j].quantity;
-                    reqArray.push(varReqObject);
-                    console.log(varReqObject);
-                }
-            }
-        }
-
-        fs.writeFileSync(`./${yearNow}-${monthNow}-${dayNow}T${hourNow}-popular.json`, JSON.stringify(reqArray));
+        fs.writeFileSync(`./${yearNow}-${monthNow}-${dayNow}T${hourNow}-popular.json`, JSON.stringify(all));
         fs.readFile('dateArr.json', function (err, data) {
             var json = JSON.parse(data)
             json.push(`./${yearNow}-${monthNow}-${dayNow}T${hourNow}-popular.json`)
@@ -283,4 +137,175 @@ function getData() {
     })).catch(errors => {
         console.error(errors);
     })
+}
+
+
+function checkBase(stockResponse, date, hourMin){
+    let checkingArr = [];
+
+    let yearNow = date.split("/")[2].split(",")[0];
+    let monthNow = date.split("/")[1];
+    let dayNow = date.split("/")[0];
+    let hourNow = date.split("/")[2].split(",")[1].replace(/\s/gm, "").split(":")[0];
+
+    for (let id1 of stockResponse) {
+        let obj = {
+            sku: "",
+            quantity: 0
+        }
+        obj.sku = id1.sku;
+        obj.quantity = id1.stocks[0].quantity;
+
+        checkingArr.push(obj);
+    }
+
+    fs.writeFileSync(`./${yearNow}-${monthNow}-${dayNow}T${hourNow}.json`, JSON.stringify(checkingArr));
+
+    let rawBase;
+
+    const getYesterday = (dateOnly = false) => {
+        let d = new Date();
+        d.setDate(d.getDate() - 1);
+          d = d.toLocaleString('en-GB', {timeZone: 'Europe/Kiev'});
+        return dateOnly ? new Date(d.toDateString()) : d;
+    };
+
+    let dayY = getYesterday().split("/")[0];
+
+    if (hourNow.toString() === "00") {
+        rawBase = fs.readFileSync(`${yearNow}-${monthNow}-${dayY}T23.json`);
+    } else if (hourNow.toString() === "01") {
+        rawBase = fs.readFileSync(`${yearNow}-${monthNow}-${dayNow}T00.json`);
+    } else {
+        console.log(hourNow);
+        rawBase = fs.readFileSync(`${yearNow}-${monthNow}-${dayNow}T${hourMin}.json`);
+    }
+
+    let baseArr = JSON.parse(rawBase);
+
+    let reqArray = []
+
+    if (baseArr.length > checkingArr.length) {
+        for (let i = 0; i < checkingArr.length; i++){
+            for(let j = 0; j < baseArr.length; j++){
+                if(checkingArr[i].sku === baseArr[j].sku && checkingArr[i].quantity > baseArr[j].quantity){
+                    let reqObject = {
+                        sku: "",
+                        diff: 0,
+                        quantity: 0
+                    }
+                    reqObject.sku = checkingArr[i].sku;
+                    reqObject.quantity = checkingArr[i].quantity;
+                    reqObject.diff = checkingArr[i].quantity - baseArr[j].quantity;
+                    reqArray.push(reqObject);
+                    console.log(reqObject);
+                }
+            }
+        }
+    } else {
+        for (let i = 0; i < baseArr.length; i++){
+            for(let j = 0; j < checkingArr.length; j++){
+                if(baseArr[i].sku === checkingArr[j].sku && baseArr[i].quantity > checkingArr[j].quantity){
+                    let reqObject = {
+                        sku: "",
+                        diff: 0,
+                        quantity: 0
+                    }
+                    reqObject.sku = baseArr[i].sku;
+                    reqObject.quantity = baseArr[i].quantity;
+                    reqObject.diff = baseArr[i].quantity - checkingArr[j].quantity;
+                    reqArray.push(reqObject);
+                    console.log(reqObject);
+                }
+            }
+        }
+    }
+
+    return reqArray;
+}
+
+function checkVariables(variableResponse, date, hourMin){
+    let checkingArr = [];
+
+    let yearNow = date.split("/")[2].split(",")[0];
+    let monthNow = date.split("/")[1];
+    let dayNow = date.split("/")[0];
+    let hourNow = date.split("/")[2].split(",")[1].replace(/\s/gm, "").split(":")[0];
+
+    for (let id1 of variableResponse) {
+        let obj = {
+            sku: "",
+            quantity: 0
+        }
+        obj.sku = id1.sku;
+        obj.quantity = id1.stocks[0].quantity;
+
+        checkingArr.push(obj);
+    }
+
+    fs.writeFileSync(`./${yearNow}-${monthNow}-${dayNow}T${hourNow}-variables.json`, JSON.stringify(checkingArr));
+
+    let rawBase;
+
+    const getYesterday = (dateOnly = false) => {
+        let d = new Date();
+        d.setDate(d.getDate() - 1);
+          d = d.toLocaleString('en-GB', {timeZone: 'Europe/Kiev'});
+        return dateOnly ? new Date(d.toDateString()) : d;
+    };
+
+    let dayY = getYesterday().split("/")[0];
+
+    if (hourNow.toString() === "00") {
+        rawBase = fs.readFileSync(`${yearNow}-${monthNow}-${dayY}T23-variables.json`);
+    } else if (hourNow.toString() === "01") {
+        rawBase = fs.readFileSync(`${yearNow}-${monthNow}-${dayNow}T00-variables.json`);
+    } else {
+        console.log(hourNow);
+        rawBase = fs.readFileSync(`${yearNow}-${monthNow}-${dayNow}T${hourMin}-variables.json`);
+    }
+
+    let baseArr = JSON.parse(rawBase);
+
+    let reqArray = []
+
+    if (baseArr.length > checkingArr.length) {
+        for (let i = 0; i < checkingArr.length; i++){
+            for(let j = 0; j < baseArr.length; j++){
+                if(checkingArr[i].sku === baseArr[j].sku && checkingArr[i].quantity > baseArr[j].quantity){
+                    let reqObject = {
+                        sku: "",
+                        diff: 0,
+                        quantity: 0
+                    }
+                    reqObject.sku = checkingArr[i].sku;
+                    reqObject.quantity = checkingArr[i].quantity;
+                    reqObject.diff = checkingArr[i].quantity - baseArr[j].quantity;
+                    reqArray.push(reqObject);
+                    console.log(reqObject);
+
+                }
+            }
+        }
+    } else {
+        for (let i = 0; i < baseArr.length; i++){
+            for(let j = 0; j < checkingArr.length; j++){
+                if(baseArr[i].sku === checkingArr[j].sku && baseArr[i].quantity > checkingArr[j].quantity){
+                    let reqObject = {
+                        sku: "",
+                        diff: 0,
+                        quantity: 0
+                    }
+                    reqObject.sku = baseArr[i].sku;
+                    reqObject.quantity = baseArr[i].quantity;
+                    reqObject.diff = baseArr[i].quantity - checkingArr[j].quantity;
+                    reqArray.push(reqObject);
+                    console.log(reqObject);
+
+                }
+            }
+        }
+    }
+
+    return reqArray;
 }
